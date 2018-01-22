42f828b1c1b83cb1ae627786a836def2bf215fdf
biglyan <l>
2018-01-22 16:26:00 +0800

    update src

--- src/example/gobyexample/atomic-counters.go
+++ src/example/gobyexample/atomic-counters.go
@@ -8,10 +8,12 @@ import (
 
 func main() {
 	var ops uint64 = 0
+	count := 0
 
-	for i := 0; i < 50; i++ {
+	for i := 0; i < 100; i++ {
 		go func() {
 			for {
+				count++
 				atomic.AddUint64(&ops, 1)
 				time.Sleep(time.Millisecond)
 			}
@@ -22,4 +24,5 @@ func main() {
 
 	opsFinal := atomic.LoadUint64(&ops)
 	fmt.Println("ops:", opsFinal)
+	fmt.Println("count:", count)
 }
---
+++ src/example/gomain/expert/assembly.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	assembly "./assembly"
+)
+
+func main() {
+	fmt.Println(assembly.Add(32, 64))
+}
